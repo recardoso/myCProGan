ae0cdd17dc884b0d79865de72dc2f2a89ef585ac
recardoso <renatocardoso1996@hotmail.com>
2021-06-04 15:31:48 +0200

    Add networks with new layers

--- layers.py
+++ layers.py
@@ -34,7 +34,7 @@ def cset(cur_lambda, new_cond, new_lambda): return lambda: tf.cond(new_cond, new
 class WeightScaleLayer(tf.keras.layers.Layer):
     def __init__(self, input_shape_value, gain = np.sqrt(2), **kwargs):
         super(WeightScaleLayer, self).__init__(**kwargs)
-        self.shape = input_shape_value
+        self.shape = input_shape_value #[kernel, kernel, x.shape[1]]
         self.fan_in = np.prod(self.shape)
         #self.wscale = gain / np.sqrt(self.fan_in)
         self.wscale = gain * tf.math.rsqrt(self.fan_in)
@@ -98,31 +98,147 @@ class Pixel_norm_layer(tf.keras.layers.Layer):
     def call(self, inputs): #rsqrt or sqrt
         return inputs * tf.math.rsqrt(tf.reduce_mean(tf.square(inputs), axis=1, keepdims=True) + self.epsilon)  
 
-def conv2dwscale(x, filters, kernel_size, gain, use_pixelnorm=False, activation=False, strides=(1,1)):
-    x = WeightScaleLayer(shape=(kernel_size[0], kernel_size[1], in_filters), gain=gain)(x)
-    x = layers.Conv2D(filters, kernel_size, strides=strides, use_bias=False, padding="same", kernel_initializer=init, dtype='float32')(x)
-    x = Bias(input_shape=x.shape)(x)
+def conv2dwscale(x, filters, kernel_size, gain, use_pixelnorm=False, activation=False, strides=(1,1), name=''):
+    init = tf.keras.initializers.RandomNormal(mean=0., stddev=1.) #change the stddev to 1
+    #[kernel, kernel, x.shape[1]]
+    x = WeightScaleLayer(input_shape_value=(kernel_size[0], kernel_size[1], x.shape[1]), gain=gain)(x)
+    x = Conv2D(filters, kernel_size, strides=strides, use_bias=False, padding="same", kernel_initializer=init, dtype='float32', data_format='channels_first', name=name)(x)
+    bias_name = name + '_bias' 
+    x = Bias(input_shape=x.shape, name=bias_name)(x)
     if activation:
-        x = layers.LeakyReLU(0.2)(x)
+        x = LeakyReLU(0.2)(x) #.2 used in the oroginal function
     if use_pixelnorm:
-        x = PixelNormalization()(x)
+        x = Pixel_norm_layer(epsilon=1e-8)(x)
     return x 
 
-def WeightScalingConv(x, filters, kernel_size, gain, use_pixelnorm=False, activate=None, strides=(1,1)):
-    init = RandomNormal(mean=0., stddev=1.)
-    in_filters = backend.int_shape(x)[-1]
-    x = layers.Conv2D(filters, kernel_size, strides=strides, use_bias=False, padding="same", kernel_initializer=init, dtype='float32')(x)
-    x = WeightScaling(shape=(kernel_size[0], kernel_size[1], in_filters), gain=gain)(x)
-    x = Bias(input_shape=x.shape)(x)
-    if activate=='LeakyReLU':
-        x = layers.LeakyReLU(0.2)(x)
-    elif activate=='tanh':
-        x = layers.Activation('tanh')(x)
-    
+def densewscale(x, filters, gain, use_pixelnorm=False, activation=False, name=''):
+    init = tf.keras.initializers.RandomNormal(mean=0., stddev=1.) #change the stddev to 1
+    #if len(x.shape) > 2: #is this necessary or the dense layer already does this?
+    #    x = tf.reshape(x, [-1, np.prod([d.value for d in x.shape[1:]])])
+    #[x.shape[1].value, fmaps]
+    x = WeightScaleLayer(input_shape_value=(x.shape[1]), gain=gain)(x)
+    x = Dense(filters, use_bias=False, kernel_initializer=init, dtype='float32', name=name)(x)
+    bias_name = name + '_bias'
+    x = Bias(input_shape=x.shape, name=bias_name)(x)
+    if activation:
+        x = LeakyReLU(0.2)(x) #.2 used in the oroginal function
     if use_pixelnorm:
-        x = PixelNormalization()(x)
+        x = Pixel_norm_layer(epsilon=1e-8)(x)
+    return x 
+
+def conv2d_downscale2dwscale(x, filters, kernel_size, gain, use_pixelnorm=False, activation=False, strides=(1,1), name=''):
+    init = tf.keras.initializers.RandomNormal(mean=0., stddev=1.) #change the stddev to 1
+    #[kernel, kernel, x.shape[1]]
+    x = WeightScaleLayer(input_shape_value=(kernel_size[0], kernel_size[1], x.shape[1]), gain=gain)(x)
+    x = Conv2D(filters, kernel_size, strides=strides, use_bias=False, padding="same", kernel_initializer=init, dtype='float32', data_format='channels_first', name=name)(x)
+    x = AveragePooling2D(pool_size=(2, 2), strides=(2, 2), padding='valid', data_format='channels_first')(x) #is this in the correct position?
+    bias_name = name + '_bias' 
+    x = Bias(input_shape=x.shape, name=bias_name)(x)
+    if activation:
+        x = LeakyReLU(0.2)(x) #.2 used in the oroginal function
+    if use_pixelnorm:
+        x = Pixel_norm_layer(epsilon=1e-8)(x)
+    return x 
+
+def upscale_conv2dwscale(x, filters, kernel_size, gain, use_pixelnorm=False, activation=False, strides=(1,1), name=''):
+    init = tf.keras.initializers.RandomNormal(mean=0., stddev=1.) #change the stddev to 1
+    #[kernel, kernel, x.shape[1]]
+    x = WeightScaleLayer(input_shape_value=(kernel_size[0], kernel_size[1], x.shape[1]), gain=gain)(x)
+    x = UpSampling2D(size=(2, 2), data_format='channels_first', interpolation='nearest')(x) #is this in the correct position?
+    x = Conv2D(filters, kernel_size, strides=strides, use_bias=False, padding="same", kernel_initializer=init, dtype='float32', data_format='channels_first', name=name)(x)
+    bias_name = name + '_bias' 
+    x = Bias(input_shape=x.shape, name=bias_name)(x)
+    if activation:
+        x = LeakyReLU(0.2)(x) #.2 used in the oroginal function
+    if use_pixelnorm:
+        x = Pixel_norm_layer(epsilon=1e-8)(x)
     return x 
 
 
-def fromrgb(a):
-    return a
+def fromrgb(x, res, filters=3, gain=np.sqrt(2)): #filters is the number of channels
+    name = '%dx%d/FromRGB_lod' % (2**res, 2**res)
+    return conv2dwscale(x, filters, kernel_size=(1,1), gain=gain, use_pixelnorm=False, activation=True, strides=(1,1), name=name)
+
+def torgb(x, res, filters=3, gain=1):
+    # lod = resolution_log2 - res
+    # with tf.variable_scope('ToRGB_lod%d' % lod):
+    name = '%dx%d/ToRGB_lod' % (2**res, 2**res)
+    return conv2dwscale(x, filters, kernel_size=(1,1), gain=gain, use_pixelnorm=False, activation=False, strides=(1,1), name=name)
+
+def encoder(x, resolution_log2, gain=np.sqrt(2)):
+    x = fromrgb(x, resolution_log2)
+    y = x
+    for res in range(resolution_log2, 4, -1):
+        name = '%dx%d/Conv_down%d' % (2**resolution_log2, 2**resolution_log2, res)
+        x = conv2d_downscale2dwscale(x, filters=128, kernel_size=(3,3), gain=gain, use_pixelnorm=False, activation=True, strides=(1,1), name=name)
+    name ='Conv_down4'
+    x = conv2d_downscale2dwscale(x, filters=32, kernel_size=(3,3), gain=gain, use_pixelnorm=False, activation=True, strides=(1,1), name=name)
+    name = 'Dense0up'
+    x = densewscale(x, 2048, gain=gain, use_pixelnorm=False, activation=True, name=name)
+    bias_name = name + '_bias_PN'
+    #x = PN(act(apply_bias(x)))
+    x = Bias(input_shape=x.shape, name=bias_name)(x)
+    x = LeakyReLU(0.2)(x)
+    x = Pixel_norm_layer(epsilon=1e-8)(x)
+    name = 'Dense1up'
+    x = densewscale(x, 2048, gain=1, use_pixelnorm=False, activation=True, name=name)
+    return x, y
+
+def nf(stage, fmap_base, fmap_decay, fmap_max): 
+    return min(int(fmap_base / (2.0 ** (stage * fmap_decay))), fmap_max)
+     
+def generator_block(x,res,y,resolution_log2,gain=np.sqrt(2)):
+    fmap_base           = 8192        # Overall multiplier for the number of feature maps.
+    fmap_decay          = 1.0          # log2 feature map reduction when doubling the resolution.
+    fmap_max            = 128          # Maximum number of feature maps in any layer.
+    scope_name = '%dx%d' % (2**res, 2**res)
+    y_temp=y
+    if res == 5: # 32x32
+        x = Pixel_norm_layer(epsilon=1e-8)(x)
+        x = tf.reshape(x, [-1, 32, 8, 8])
+        bias_name = scope_name + '_Dense_bias_PN'
+        #x = PN(act(apply_bias(x)))
+        x = Bias(input_shape=x.shape, name=bias_name)(x)
+        x = LeakyReLU(0.2)(x)
+        x = Pixel_norm_layer(epsilon=1e-8)(x)
+        name = scope_name + '/Conv'
+        x = upscale_conv2dwscale(x, filters=64, kernel_size=(3,3), gain=gain, use_pixelnorm=True, activation=True, strides=(1,1), name=name)
+    else: #x64 and up
+        for r in range(resolution_log2, res-1, -1):
+            name = scope_name + 'Conv_down%d_%d' %(res, r)
+            y_temp= conv2d_downscale2dwscale(y_temp, filters=nf(res-5, fmap_base, fmap_decay, fmap_max), kernel_size=(3,3), gain=gain, use_pixelnorm=False, activation=True, strides=(1,1), name=name)
+        x = UpSampling2D(size=(2, 2), data_format='channels_first', interpolation='nearest')(x)
+        x = tf.concat([x, y_temp], axis=1)
+        name = scope_name + '/ConvConcat'
+        x = conv2dwscale(x, filters=nf(res-5, fmap_base, fmap_decay, fmap_max), kernel_size=(3,3), gain=gain, use_pixelnorm=True, activation=True, strides=(1,1), name=name)
+        name = scope_name + '/Conv0'
+        x = conv2dwscale(x, filters=nf(res-5, fmap_base, fmap_decay, fmap_max), kernel_size=(3,3), gain=gain, use_pixelnorm=True, activation=True, strides=(1,1), name=name)
+        name = scope_name + '/Conv1'
+        x = conv2dwscale(x, filters=nf(res-5, fmap_base, fmap_decay, fmap_max), kernel_size=(3,3), gain=gain, use_pixelnorm=True, activation=True, strides=(1,1), name=name)
+    return x
+
+def discriminator_block(x,res,resolution_log2,gain=np.sqrt(2),mbstd_group_size = 4):
+    fmap_base           = 8192         # Overall multiplier for the number of feature maps.
+    fmap_decay          = 1.0          # log2 feature map reduction when doubling the resolution.
+    fmap_max            = 512          # Maximum number of feature maps in any layer.
+    label_size          = 0
+    scope_name = '%dx%d' % (2**res, 2**res)
+    if res >= 3: # 8x8 and up
+        name = scope_name + '/Conv0'
+        x = conv2dwscale(x, nf(res-1, fmap_base, fmap_decay, fmap_max), kernel_size=(3,3), gain=gain, use_pixelnorm=False, activation=True, strides=(1,1), name=name)
+        #fused?
+        name = scope_name + '/Conv1_down'
+        x = conv2d_downscale2dwscale(x, filters=nf(res-2, fmap_base, fmap_decay, fmap_max), kernel_size=(3,3), gain=gain, use_pixelnorm=False, activation=True, strides=(1,1), name=name)
+    else: # 4x4
+        if mbstd_group_size > 1:
+            Minibatch_stddev_layer(mbstd_group_size)(x)
+        name = scope_name + '/Conv0'
+        x = conv2dwscale(x, filters=nf(res-1, fmap_base, fmap_decay, fmap_max), kernel_size=(3,3), gain=gain, use_pixelnorm=False, activation=True, strides=(1,1), name=name)
+        name = scope_name + '/Dense0'
+        x = densewscale(x, filters=nf(res-2, fmap_base, fmap_decay, fmap_max), use_pixelnorm=False, activation=True, name=name)
+
+        name = scope_name + '/Dense1'
+        x = densewscale(x, filters=1+label_size, gain=1, use_pixelnorm=False, activation=True, name=name)     
+    return x
+
+        
--- networks2.py
+++ networks2.py
@@ -33,18 +33,59 @@ def generator(resolution=256,num_channels=3,num_replicas=1): #confirm params and
 
     resolution_log2 = int(np.log2(resolution))
     dshape=( 3, resolution, resolution) 
-    normal_gain = np.sqrt(2)
     latent = Input(shape=dshape)
     latent = tf.cast(latent, tf.float32)
+    lod_in = Input(shape=(), batch_size=num_replicas, name='lod_in')
+    lod_in = tf.cast(lod_in, tf.float32)
+    dtype = 'float32'
+
+    x,y = encoder(latent, resolution_log2, gain=np.sqrt(2))
+
+
+
+    x = generator_block(x, 5, y, resolution_log2)
+    images_out = torgb(x, 5)
+    for res in range(6, resolution_log2 + 1):
+        lod = resolution_log2 - res
+        x = generator_block(x, res, y, resolution_log2)
+        img = torgb(x, res)
+        images_out = UpSampling2D(size=(2, 2), data_format='channels_first', interpolation='nearest')(images_out)
+        images_out = Lerp_clip_layer()(img, images_out, lod_in - lod)
+        #lerp_clip(img, images_out, lod_in - lod)
+
+    assert images_out.dtype == tf.as_dtype(dtype)
+    images_out = tf.identity(images_out, name='images_out')
+    
+    Model(inputs=[latent, lod_in], outputs=[images_out]).summary()
+    
+    return Model(inputs=[latent, lod_in], outputs=[images_out])
+
+def discriminator(resolution=256, num_channels=3, label_size = 0, mbstd_group_size = 4, old_res=128, num_replicas=1):
+    resolution_log2 = int(np.log2(resolution))
+    dshape=( num_channels, resolution, resolution) 
+    normal_gain = np.sqrt(2)
+    images_in = Input(shape=dshape)
+    images_in = tf.cast(images_in, tf.float32)
     lod_in= Input(shape=(), batch_size=num_replicas, name='lod_in')
     lod_in = tf.cast(lod_in, tf.float32)
 
+    #TODO: Final 2 dense layers too sharp: add one or more dense layers in the middle
+    dtype               = 'float32'
 
-    fmap_base           = 8192        # Overall multiplier for the number of feature maps.
-    fmap_decay          = 1.0         # log2 feature map reduction when doubling the resolution.
-    fmap_max            = 128         # Maximum number of feature maps in any layer.
+    img = images_in
+    x = fromrgb(img, resolution_log2)
+    for res in range(resolution_log2, 2, -1):
+        lod = resolution_log2 - res
+        x = discriminator_block(x, res, resolution_log2, mbstd_group_size = mbstd_group_size)
+        img = AveragePooling2D(pool_size=(2, 2), strides=(2, 2), padding='valid', data_format='channels_first')(img)
+        y = fromrgb(img, res - 1)
+        x = Lerp_clip_layer()(x, y, lod_in - lod)
+    combo_out = discriminator_block(x, 2, resolution_log2, mbstd_group_size = mbstd_group_size)
 
-    def nf(stage): 
-        fm = min(int(fmap_base / (2.0 ** (stage * fmap_decay))), fmap_max)
-        return fm
+    assert combo_out.dtype == tf.as_dtype(dtype)
+    scores_out = tf.identity(combo_out[:, :1], name='scores_out')
+    #labels_out = tf.identity(combo_out[:, 1:], name='labels_out')
 
+    Model(inputs=[images_in, lod_in], outputs=[scores_out]).summary()
+    
+    return Model(inputs=[images_in, lod_in], outputs=[scores_out])
