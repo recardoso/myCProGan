482a4b73d2fe08dacacc410fa090f1d29595cb0e
recardoso <renatocardoso1996@hotmail.com>
2021-09-03 10:52:16 +0200

    add big image generation

--- results_processing.py
+++ results_processing.py
@@ -188,10 +188,199 @@ def cvae_plot_loss():
     # fig.savefig("losses.png")
     return
 
+def get_images_grid(n_images):
+    images= []
+    for imgi in range(n_images):
+        filename = 'cond4/'+str(imgi+1)+'.png'
+        #filename = 'img.png'
+        if os.path.isfile(filename):
+            im=Image.open(filename)
+            im.load()
+            im = np.asarray(im, dtype=np.float32 )
+            im=np.transpose(im, (2, 0, 1))
+        images.append(im)
+
+    if n_images > 1:
+        for imgi in range(n_images-1):
+            filename = 'cond4/'+str(imgi*5+6)+'.png'
+            #filename = 'img.png'
+            if os.path.isfile(filename):
+                im=Image.open(filename)
+                im.load()
+                im = np.asarray(im, dtype=np.float32 )
+                im=np.transpose(im, (2, 0, 1))
+            images.append(im)
+
+
+    return images
+
+
+def setup_image_grid(dataset_shape, n_images=1, m_size= '1080p'):
+
+    # Select size
+    gw = n_images; gh = n_images
+    # if m_size == '1080p':
+    #     gw = np.clip(1920 // dataset_shape[2], 3, 32)
+    #     gw = gw - (gw % 2)
+    #     gh = np.clip(1080 // dataset_shape[1], 2, 32)
+    # if m_size == '4k':
+    #     gw = np.clip(3840 // dataset_shape[2], 7, 32)
+    #     gw = gw - (gw % 2)
+    #     gh = np.clip(2160 // dataset_shape[1], 4, 32)
+
+    #size = dataset_shape
+    #size = int(size / 2)
+    size = 128 #dataset_shape
+
+    #get images
+    images = get_images_grid(n_images)
+
+    img_counter = 0
+
+    # Fill in reals and labels.
+    #reals = np.zeros([int(gw * gh)] + dataset_shape, dtype=np.float32)
+    #fakes = np.zeros([int((gw / 2) * gh)] + dataset_shape, dtype=np.float32)
+    grid = np.zeros([(gw * 2) * (gh * 2)] + dataset_shape, dtype=np.float32)
+    for idx in range(gw * gh):
+        real = images[img_counter]
+        img_counter += 1
+
+        if (idx < n_images) or (idx % n_images == 0):
+            corner1= real[:, :(size),:(size)]
+            corner2= real[:, (size):,:(size)]
+            corner3= real[:, :(size),(size):]
+            corner4= real[:, (size):,(size):]
+
+            corner1 = adjust_dynamic_range(corner1, [0,255], [-1,1])
+            corner2 = adjust_dynamic_range(corner2, [0,255], [-1,1])
+            corner3 = adjust_dynamic_range(corner3, [0,255], [-1,1])
+            corner4 = adjust_dynamic_range(corner4, [0,255], [-1,1])
+
+            grid[idx*2] = corner1
+            grid[idx*2+1] = corner2
+            grid[idx*2 + n_images * 2] = corner3
+            grid[idx*2 + n_images * 2 +1] = corner4
+
+        if (idx % n_images == 0) and idx != 0:
+            corner1= real[:, :(size),:(size)]
+            corner2= real[:, (size):,:(size)]
+            corner3= real[:, :(size),(size):]
+            corner4= real[:, (size):,(size):]
+
+            corner1 = adjust_dynamic_range(corner1, [0,255], [-1,1])
+            corner2 = adjust_dynamic_range(corner2, [0,255], [-1,1])
+            corner3 = adjust_dynamic_range(corner3, [0,255], [-1,1])
+            corner4 = adjust_dynamic_range(corner4, [0,255], [-1,1])
+
+            grid[idx*2 *2] = corner1
+            grid[idx*2 *2 +1] = corner2
+            grid[idx*2 *2 + n_images * 2] = corner3
+            grid[idx*2 *2 + n_images * 2 +1] = corner4
+
+
+
+        # corner1= real[:, :(size),:(size)]
+        # corner2= real[:, (size):,:(size)]
+        # corner3= real[:, :(size),(size):]
+        # corner4= np.zeros([3, 128, 128])
+
+        # image_right = tf.concat([corner3, corner4], axis=1)
+        # image_left = tf.concat([corner1, corner2], axis=1)
+        # image_full = tf.concat([image_left, image_right], axis=2)
+
+        #grid[idx] = image_full
+        #reals[idx] = real
+
+    # Generate latents.
+    return gw, gh, grid
+
+def generate_big_image(n_images, cvae_model, gen_model, lod_in = 0.0):
+    gw, gh, grid = setup_image_grid(dataset_shape=(3,128,128), n_images=1, m_size= '1080p')
+    total_n_images = n_images * 2 * n_images * 2
+    for imgi in range(total_n_images):
+        if imgi > n_images * 2 and imgi < (total_n_images - n_images) and (imgi % (n_images * 2)) != 0 and imgi % (n_images * 2) != (n_images * 2 - 1):
+
+            corner1 = np.zeros((1, 3, 128, 128))
+            corner2 = np.zeros((1, 3, 128, 128))
+            corner3 = np.zeros((1, 3, 128, 128))
+            #corner4 = np.zeros((1, 3, 128, 128))
+
+            corner1[0]= grid[imgi]
+            corner2[0]= grid[imgi + 1]
+            corner3[0]= grid[imgi + n_images * 2]
+            #corner4= grid[imgi + + n_images * 2 +1]
+
+            noise_shape = 512
+
+            #generate noise image
+            latents = tf.random.normal([1, noise_shape])
+
+            ae_latent1 = cvae_model.encode(corner1)
+            ae_latent2 = cvae_model.encode(corner2)
+            ae_latent3 = cvae_model.encode(corner3)
+
+            #check axis
+            ae_latent_left = tf.concat([ae_latent1, ae_latent2], axis=1)
+            ae_latents = tf.concat([ae_latent_left, ae_latent3], axis=1)
+
+            corner4 = tf.cast(gen_model([latents, ae_latents, lod_in], training=False), tf.float32) 
+
+            image_right = tf.cast(tf.concat([corner3, corner4], axis=2), tf.float32)
+            image_left = tf.cast(tf.concat([corner1, corner2], axis=2), tf.float32)
+            image_full = tf.concat([image_left, image_right], axis=3)
+
+            predictions = image_full.numpy()
+
+            grid_fakes = predictions[0]
+
+            grid_fakes = adjust_dynamic_range(grid_fakes, [-1,1], [0,255])
+
+            grid[imgi + + n_images * 2 +1] = grid_fakes#.transpose(1, 2, 0) # CHW -> HWC
+
+            #image = adjust_dynamic_range(image, drange_net, drange_data)
+
+        grid_to_save = []
+
+    for el in grid:
+        #gridi = el.transpose(1, 2, 0) 
+        grid_to_save.append(el)
+
+    return gw, gh, grid_to_save
+
+def save_grid(gw, gh,grid,dataset_shape=None,step=0):
+    num, img_w, img_h = len(grid), grid[0].shape[2], grid[0].shape[1]
+    #print(num, img_w, img_h)
+
+
+    save_grid = np.zeros([grid[0].shape[0]] + [gh * 2 * img_h, gw * 2 * img_w], dtype=np.float32)
+    for idx in range(num):
+        x = (idx % gw) * img_w
+        y = (idx // gw) * img_h
+        save_grid[..., y : y + img_h, x : x + img_w] = grid[idx]
+
+    image = save_grid.transpose(1, 2, 0) 
+    image = np.rint(image).clip(0, 255).astype(np.uint8)
+    format = 'RGB'
+    image = Image.fromarray(image, format).save('generated_image.png')
+
+    return save_grid
+
 
 if __name__ == "__main__":
     #plot_loss()
-    cvae_plot_loss()
+    #cvae_plot_loss()
+
+    gen = networks2.generator(256, num_replicas = 1)
+    gen.built = True
+    gen.load_weights('models/generator_8000.h5')
+    cvae = networks2.CVAE(resolution=256, base_filter=32,latent_dim=1024)
+    cvae.built = True #subcalssed model needs to be built use tf format instead of hdf5 might solve the problem
+    cvae.load_weights('saved_models/cvae_Final.h5')
+
+    n_images = 1
+
+    gw, gh, grid_to_save = generate_big_image(n_images, cvae, gen, lod_in = 0.0)
+    save_grid(gw, gh, grid_to_save, dataset_shape=None,step=0)
         
     # gen = networks2.generator(256)
     # step = 640
